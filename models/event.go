d1a02da8d5a493f01f4d618749cce0c2b22bce1a
RyanYoung72443 <sryoung72443@gmail.com>
2024-04-24 12:59:06 -0400

    clean up functions

--- models/event.go
+++ models/event.go
@@ -15,8 +15,6 @@ type Event struct {
 	UserID      int64
 }
 
-var events = []Event{}
-
 func (e *Event) Save() error {
 	query := `
 	INSERT INTO events(name, description, location, dateTime, user_id) 
